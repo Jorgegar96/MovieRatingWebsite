633521a33f705401a1a6a45507691067ffc9ae4c
Jorge Garcia <jorgegar896@gmail.com>
2020-12-12 18:34:34 -0600

    Bootstrap added, in progress of styling navbar

--- movie-rating/src/components/Nav.tsx
+++ movie-rating/src/components/Nav.tsx
@@ -2,17 +2,33 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Nav = () => {
+    const navStyle = {
+        color: 'white'
+    };
     return(
-        <nav>
-            <h3>Logo Here</h3>
-            <ul>
-                <Link to='/About'>
-                    <li>About</li>
-                </Link>
-                <Link to='/'>
-                    <li>Logout</li>
-                </Link>
-            </ul>
+        <nav className="navbar navbar-light navbar-expand-lg bg-dark scrolling-navbar">
+            <h3 className="">Rate The Movie</h3>
+            <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportContent" 
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <Link to='/About'>
+                        <li className="nav-item active">About</li>
+                    </Link>
+                    <Link to='/'>
+                        <li className="nav-item">Logout</li>
+                    </Link>
+                </ul>
+            </div>
         </nav>
     )
 }
